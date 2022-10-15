bfc2320e352d5c4d589b80672cbacf2eb95ecb3d
Mehmet Duran ATASEVEN <ataseven.md@gmail.com>
2022-10-15 20:36:07 +0300

    Add files via upload

---
+++ socialty/index.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html lang="tr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/main.css" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+
+    <title>SOCIALTY</title>
+</head>
+<body>
+
+    <div id="page-container">
+        <div id="page-bar">
+            <div class="navigation-socialty-logo">
+                <img src="img/Sociality_Logo_MonochromaticV_positive.svg" alt="Sociality Logo" />
+            </div>
+            <div class="navigation-content">
+
+                <div class="navigation-client">
+                    <div class="clients"><img src="img/client-1.png" alt="Client-1" /></div>
+                    <div class="clients" data-notify="99"><img src="img/client-2.png" alt="Client-2" /></div>
+                    <div class="clients"><img src="img/client-3.png" alt="Client-3" /></div>
+                    <div class="clients selected"><img src="img/client-4.png" alt="Client-4" /></div>
+                    <div class="clients"><img src="img/client-5.png" alt="Client-5" /></div>
+                    <div class="clients"><img src="img/client-6.png" alt="Client-6" /></div>
+                </div>
+                <div class="navigation-menu">
+                    <button class="menu-item">
+                        <svg class="menu-icons" viewBox="0 0 20 18">
+                            <path class="cls-1" d="M15.267,14.157H4.661V13.251l1.3-1.334V9.3A3.986,3.986,0,0,1,9.127,5.332V4.95a0.838,0.838,0,1,1,1.674,0V5.332A3.987,3.987,0,0,1,13.965,9.3v2.613l1.3,1.334v0.906Zm-5.3,2.235A1.222,1.222,0,0,1,8.8,15.235h2.325A1.222,1.222,0,0,1,9.964,16.392Zm6.225-3.74-1.144-1.173V9.3a5.081,5.081,0,0,0-3.21-4.784,1.91,1.91,0,0,0-3.738,0A5.081,5.081,0,0,0,4.885,9.3v2.175L3.742,12.652a0.553,0.553,0,0,0-.159.388V14.7a0.54,0.54,0,0,0,.54.539h3.6a2.3,2.3,0,0,0,2.24,2.234,2.3,2.3,0,0,0,2.242-2.234h3.6a0.54,0.54,0,0,0,.54-0.539V13.039A0.559,0.559,0,0,0,16.189,12.652Z" />
+                            <path class="cls-2" d="M13.444,2.377l-0.263.64a0.231,0.231,0,0,0,.121.3,5.2,5.2,0,0,1,3,4.457,0.23,0.23,0,0,0,.23.223l0.69,0a0.23,0.23,0,0,0,.23-0.237,6.358,6.358,0,0,0-3.705-5.5A0.229,0.229,0,0,0,13.444,2.377Zm0.873-2.133-0.261.64a0.23,0.23,0,0,0,.122.3,7.516,7.516,0,0,1,4.43,6.577,0.23,0.23,0,0,0,.23.225l0.69,0a0.231,0.231,0,0,0,.23-0.236A8.671,8.671,0,0,0,14.624.119,0.231,0.231,0,0,0,14.318.244ZM6.556,2.377l0.263,0.64a0.231,0.231,0,0,1-.121.3,5.2,5.2,0,0,0-3,4.457A0.23,0.23,0,0,1,3.465,8l-0.69,0a0.23,0.23,0,0,1-.23-0.237,6.358,6.358,0,0,1,3.705-5.5A0.229,0.229,0,0,1,6.556,2.377ZM5.682,0.244l0.261,0.64a0.23,0.23,0,0,1-.122.3,7.516,7.516,0,0,0-4.43,6.577,0.23,0.23,0,0,1-.23.225l-0.69,0A0.231,0.231,0,0,1,.24,7.743,8.671,8.671,0,0,1,5.375.119,0.231,0.231,0,0,1,5.682.244Z" />
+                        </svg>
+                        <label>NOTIFICATIONS</label>
+                        <span class="count-bar">29</span>
+                    </button>
+
+                    <div class="menu-block">
+                        <button class="menu-item">
+                            <svg class="menu-icons" viewBox="0 0 20 18">
+                                <path class="cls-2" d="M17.909,11.271h-0.9v-8.7h0.9a0.685,0.685,0,0,0,0-1.369H2.1a0.685,0.685,0,0,0,0,1.369H3v8.7H2.1a0.685,0.685,0,0,0,0,1.369h5.57c-0.014.012-.029,0.021-0.042,0.035L4.689,15.621a0.693,0.693,0,0,0,0,.969,0.67,0.67,0,0,0,.955,0l2.935-2.945a0.693,0.693,0,0,0,0-.97c-0.014-.015-0.031-0.025-0.046-0.038h2.942c-0.016.013-.032,0.024-0.047,0.039a0.692,0.692,0,0,0,0,.968l2.935,2.946a0.669,0.669,0,0,0,.955-0.005,0.691,0.691,0,0,0,0-.968l-2.934-2.945h0c-0.013-.013-0.028-0.023-0.042-0.035h5.568A0.685,0.685,0,0,0,17.909,11.271Zm-2.249-8.7v8.7H4.347v-8.7H15.66Z" />
+                                <path class="cls-1" d="M6.556,8.6L8.709,6.653,10.9,8.782A0.77,0.77,0,0,0,11.441,9l0.051,0a0.774,0.774,0,0,0,.518-0.248l1.649-1.8a0.787,0.787,0,0,0,.435.13,0.766,0.766,0,0,0,.76-0.77L14.844,5.36A0.762,0.762,0,0,0,14.593,4.8a0.786,0.786,0,0,0-.583-0.2l-0.96.08a0.766,0.766,0,0,0-.523.268,0.754,0.754,0,0,0-.007.972L11.409,7.141,9.264,5.06a0.773,0.773,0,0,0-1.057-.018L5.519,7.477a0.758,0.758,0,0,0-.049,1.076A0.777,0.777,0,0,0,6.556,8.6Z" />
+                            </svg>
+                            <label>SUMMARY</label>
+                            <span class="collapse"></span>
+                        </button>
+                        <ul type="circle">
+                            <li>Compose</li>
+                            <li>Feed</li>
+                        </ul>
+                    </div>
+
+                    <div class="menu-block">
+                        <button class="menu-item active">
+                            <svg class="menu-icons" viewBox="0 0 20 18">
+                                <path class="cls-1" d="M18.337,4.541L9.9,12.426a0.37,0.37,0,0,1-.253.1h-2.5a0.346,0.346,0,0,1-.357-0.334V9.853a0.323,0.323,0,0,1,.1-0.236l8.442-7.885a0.86,0.86,0,0,1,1.131,0l1.876,1.752A0.712,0.712,0,0,1,18.337,4.541ZM7.5,11.856h2l-2-1.864v1.864ZM8,9.52l2,1.864,6.463-6.037-2-1.864Zm9.828-5.563L15.956,2.2a0.077,0.077,0,0,0-.12,0l-0.864.807,2,1.864,0.864-.807A0.076,0.076,0,0,0,17.832,3.956Zm0.505,0.584L9.9,12.426" />
+                                <path class="cls-2" data-name="Icon3" class="cls-2" d="M2.139,2.531V15.484a0.35,0.35,0,0,0,.357.341H16.075a0.35,0.35,0,0,0,.357-0.341V9.008h0.715v6.477a1.049,1.049,0,0,1-1.072,1.023H2.5a1.049,1.049,0,0,1-1.072-1.023V2.531A1.049,1.049,0,0,1,2.5,1.508h9.648V2.19H2.5A0.35,0.35,0,0,0,2.139,2.531ZM17.147,9.008v6.477" />
+                            </svg>
+                            <label>PUBLISH</label>
+                            <span class="collapse"></span>
+                        </button>
+                        <ul type="circle">
+                            <li>Compose</li>
+                            <li>Feed</li>
+                        </ul>
+                    </div>
+
+                    <div class="menu-block">
+                        <button class="menu-item">
+                            <svg class="menu-icons" viewBox="0 0 20 18">
+                                <path class="cls-1" d="M3.442,5.827A5.9,5.9,0,0,0,0,10.929a5.212,5.212,0,0,0,1.085,3.141l-0.649,2a0.4,0.4,0,0,0,.1.408,0.426,0.426,0,0,0,.42.1l2.233-.684A8.688,8.688,0,0,0,7.5,17a7.732,7.732,0,0,0,6.844-3.608,5.806,5.806,0,0,0-1.146.248,7.161,7.161,0,0,1-5.7,2.55,7.806,7.806,0,0,1-4.035-1.082,0.427,0.427,0,0,0-.345-0.042l-1.649.5,0.473-1.457a0.4,0.4,0,0,0-.07-0.371A4.469,4.469,0,0,1,.833,10.929a4.942,4.942,0,0,1,2.5-4.1c0,0.019-.022.051,0,0.056A1.892,1.892,0,0,0,3.442,5.827Zm10.9,7.565a9.116,9.116,0,0,0,2.065-.86l2.632,0.806a0.424,0.424,0,0,0,.42-0.1,0.4,0.4,0,0,0,.1-0.408L18.79,10.445A5.991,5.991,0,0,0,20,6.881C20,3.087,16.262,0,11.667,0,7.506,0,4.059,2.533,3.442,5.827a3.272,3.272,0,0,1-.105,1c0,0.019,0,.037,0,0.056,0,3.794,3.738,6.881,8.333,6.881A10.04,10.04,0,0,0,13.2,13.64m3.282-1.935a0.429,0.429,0,0,0-.345.042,8.636,8.636,0,0,1-4.468,1.205c-4.135,0-7.5-2.724-7.5-6.072S7.531,0.81,11.667.81s7.5,2.724,7.5,6.071A5.242,5.242,0,0,1,18,10.125a0.4,0.4,0,0,0-.066.367l0.6,1.84Z" />
+                            </svg>
+                            <label>ENGAGE</label>
+                            <span class="collapse"></span>
+                        </button>
+                        <ul type="circle">
+                            <li>Compose</li>
+                            <li>Feed</li>
+                        </ul>
+                    </div>
+
+                    <div class="menu-block">
+                        <button class="menu-item">
+                            <svg class="menu-icons" viewBox="0 0 20 18">
+                                <path class="cls-2" d="M1.535,10.027H3.373a0.568,0.568,0,0,1,.4.231,1.714,1.714,0,0,1,.34.844,2.726,2.726,0,0,0,.628,1.447,1.623,1.623,0,0,0,1.207.576A1.576,1.576,0,0,0,7.094,12.6a2.676,2.676,0,0,0,.651-1.351L8.4,7.9a1.47,1.47,0,0,1,.366-0.791l0.047-.034,0.235-.134H9.113a0.5,0.5,0,0,1,.394.2A1.589,1.589,0,0,1,9.855,8l1.164,7.979a2.679,2.679,0,0,0,.618,1.45,1.552,1.552,0,0,0,2.411,0,2.68,2.68,0,0,0,.62-1.45L15.386,11.1a1.708,1.708,0,0,1,.331-0.831,0.582,0.582,0,0,1,.411-0.241h1.718a0.558,0.558,0,0,0,.386-0.953,0.538,0.538,0,0,0-.384-0.164H16.128a1.691,1.691,0,0,0-1.256.637,2.866,2.866,0,0,0-.58,1.39l-0.707,4.88a1.6,1.6,0,0,1-.348.861,0.518,0.518,0,0,1-.394.212,0.538,0.538,0,0,1-.384-0.2,1.609,1.609,0,0,1-.358-0.872L10.95,7.837a2.73,2.73,0,0,0-.628-1.45,1.644,1.644,0,0,0-1.207-.566,1.62,1.62,0,0,0-1.158.515A2.671,2.671,0,0,0,7.32,7.684L6.667,11.037a1.6,1.6,0,0,1-.345.781l-0.03.034-0.3.173L5.888,12a0.6,0.6,0,0,1-.333-0.193A1.706,1.706,0,0,1,5.2,10.943a2.691,2.691,0,0,0-.583-1.4,1.637,1.637,0,0,0-1.243-.634H1.535a0.555,0.555,0,0,0-.549.559A0.548,0.548,0,0,0,1.535,10.027Z" />
+                                <path class="cls-1" d="M18.708,8.236H16.847a0.579,0.579,0,0,1-.408-0.228A1.667,1.667,0,0,1,16.1,7.176a2.657,2.657,0,0,0-.636-1.428,1.661,1.661,0,0,0-1.222-.569,1.614,1.614,0,0,0-1.158.516,2.613,2.613,0,0,0-.659,1.333l-0.659,3.309a1.436,1.436,0,0,1-.371.78l-0.048.034-0.238.132h-0.07a0.51,0.51,0,0,1-.4-0.2,1.546,1.546,0,0,1-.352-0.849L9.106,2.364A2.61,2.61,0,0,0,8.48.933a1.6,1.6,0,0,0-2.442,0,2.611,2.611,0,0,0-.628,1.431L4.684,7.18A1.662,1.662,0,0,1,4.349,8a0.593,0.593,0,0,1-.416.238H2.194a0.554,0.554,0,0,0-.556.551,0.544,0.544,0,0,0,.165.388,0.552,0.552,0,0,0,.389.162h1.74A1.725,1.725,0,0,0,5.2,8.711a2.79,2.79,0,0,0,.587-1.372L6.507,2.525a1.563,1.563,0,0,1,.352-0.849,0.529,0.529,0,0,1,.4-0.209,0.55,0.55,0,0,1,.389.2,1.568,1.568,0,0,1,.362.861L9.175,10.4a2.66,2.66,0,0,0,.636,1.43,1.683,1.683,0,0,0,1.222.558,1.659,1.659,0,0,0,1.172-.508,2.607,2.607,0,0,0,.646-1.33L13.512,7.24a1.564,1.564,0,0,1,.349-0.77l0.03-.033L14.2,6.265,14.3,6.29a0.611,0.611,0,0,1,.337.191A1.663,1.663,0,0,1,15,7.333a2.621,2.621,0,0,0,.591,1.381,1.672,1.672,0,0,0,1.258.625h1.861a0.554,0.554,0,0,0,.556-0.551A0.548,0.548,0,0,0,18.708,8.236Z" />
+                            </svg>
+                            <label>LISTEN</label>
+                            <span class="collapse"></span>
+                        </button>
+                        <ul type="circle">
+                            <li>Compose</li>
+                            <li>Feed</li>
+                        </ul>
+                    </div>
+
+                    <div class="menu-block">
+                        <button class="menu-item">
+                            <svg class="menu-icons" viewBox="0 0 20 18">
+                                <path class="cls-1" d="M1.344,18V17.25h18V18h-18Zm16.125-1.5h-2.25a0.75,0.75,0,0,1-.75-0.75V5.625a0.75,0.75,0,0,1,.75-0.75h2.25a0.75,0.75,0,0,1,.75.75V15.75A0.75,0.75,0,0,1,17.469,16.5Zm0-10.875h-2.25V6.75h2.25V5.625Zm0,1.875h-2.25v8.25h2.25V7.5Zm-6,1.875a0.75,0.75,0,0,1,.75.75V15.75a0.75,0.75,0,0,1-.75.75H9.219a0.75,0.75,0,0,1-.75-0.75V10.125a0.75,0.75,0,0,1,.75-0.75h2.25ZM9.219,15.75h2.25V12H9.219v3.75Zm0-4.5h2.25V10.125H9.219V11.25Zm-3.75-3a0.75,0.75,0,0,1,.75.75v6.75a0.75,0.75,0,0,1-.75.75H3.219a0.75,0.75,0,0,1-.75-0.75V9a0.75,0.75,0,0,1,.75-0.75h2.25Zm-2.25,7.5h2.25V10.875H3.219V15.75Zm0-5.625h2.25V9H3.219v1.125ZM16.344,1.28L11.257,6.367a1.293,1.293,0,0,1-1.827,0L6.6,3.538a0.543,0.543,0,0,0-.766,0L2.254,7.12l-0.53-.53L5.305,3.008a1.293,1.293,0,0,1,1.827,0L9.96,5.836a0.543,0.543,0,0,0,.766,0L15.813,0.75h-1.72V0h2.625a0.375,0.375,0,0,1,.375.375V3h-0.75V1.28Z" />
+                            </svg>
+                            <label>REPORT</label>
+                            <span class="collapse"></span>
+                        </button>
+                        <ul type="circle">
+                            <li>Compose</li>
+                            <li>Feed</li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+
+        <div id="page-content">
+            <div id="page-headbar">
+                <div class="description">
+                    <span class="desc-item"><i class="desc-color" style="--desc-color: #acacac;"></i> Published</span>
+                    <span class="desc-item"><i class="desc-color" style="--desc-color: #3ac183;"></i> Scheduled</span>
+                    <span class="desc-item"><i class="desc-color" style="--desc-color: #f7bf38;"></i> Need Approval</span>
+                    <span class="desc-item"><i class="desc-color" style="--desc-color: #fb6450;"></i> Error</span>
+                    <span class="desc-item"><i class="desc-color" style="--desc-color: #67b1f2;"></i> Notes</span>
+                </div>
+                <img src="img/avatar.png" id="avatar" />
+            </div>
+
+
+            <?php
+            function getStatusColor($value)
+            {
+                switch ($value) {
+                    case "0":
+                        return "need-approval";
+                        break;
+                    case "1":
+                        return "scheduled";
+                        break;
+                    case "2":
+                        return "notes";
+                        break;
+                    case "3":
+                        return "published";
+                        break;
+                    case "4":
+                        return "error";
+                        break;
+                }
+            }
+
+            $json = file_get_contents('data.json');
+            $json_data = json_decode($json, true);
+            ?>
+
+            <?php foreach ($json_data["posts_by_date"] as $post_date => $post_content) { ?>
+                <div class="post-section">
+                    <div class="post-date"><?php $date = date_create($post_date);
+                                            echo date_format($date, "d F Y"); ?></div>
+
+                    <div class="post-bar">
+                        <?php foreach ($post_content as $key => $val) {
+                        ?>
+                            <div class="post-card">
+                                <div class="color-box <?= getStatusColor($val["status"]) ?>"><img class="post-icon" src="img/<?= $val["account"]["channel"] ?>.png"></div>
+                                <div class="card">
+                                    <div class="header">
+                                        <span class="date"><?php $date = date_create($val["published_at"]);
+                                                            echo date_format($date, "d F Y - H:i"); ?></span>
+                                        <div class="actions">
+                                            <img src="img/<?php echo $val["is_published"] == "1" ? "approve" : "cancel"; ?>.png" />
+                                            <img src="img/delete.png" />
+                                            <img src="img/options.png" />
+                                        </div>
+                                    </div>
+                                    <div class="content"><?= $val["entry"]["message"] ?></div>
+                                    <div class="content-image"><img src="<?= $val["entry"]["image"][0] ?>" onerror="imgError(this);" /></div>
+                                    <div class="reactions">
+                                        <div class="reaction likes"><img src="img/<?= $val["account"]["channel"] == "twitter" ? "heart" : "likes" ?>.png"><span>0</span></div>
+                                        <?php echo $val["account"]["channel"] == "twitter" ? "<div class='reaction shares-twitter'><img src='img/shares-twitter.png'><span>0</span></div>" : null ?>
+                                        <div class="reaction comments"><img src="img/comments.png"><span>0</span></div>
+                                        <?php echo $val["account"]["channel"] != "twitter" ? "<div class='reaction shares'><img src='img/shares.png'><span>0</span></div>" : null ?>
+                                        <div class="reaction views"><img src="img/views.png"><span>0</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            <?php } ?>
+
+
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script>
+        $(".navigation-client .clients").click(function() {
+            $(".navigation-client .clients").removeClass("selected");
+            $(this).toggleClass("selected");
+        });
+        $(".menu-block .menu-item").click(function() {
+            $(".menu-block .menu-item").removeClass("active");
+            $(this).toggleClass("active");
+        });
+
+        function imgError(image) {
+            image.onerror = "";
+            image.src = "img/no-post-image.png";
+            return true;
+        }
+    </script>
+
+</body>
+
+</html>
